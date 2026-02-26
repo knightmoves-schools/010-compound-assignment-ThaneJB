0909a9db23b094700f18f8e5fa96e1fe65730f3e
Thane <thabay0606@students.maquoketaschools.org>
2026-02-26 13:53:13 -0600

    Removed int

--- Calculator.cs
+++ Calculator.cs
@@ -8,13 +8,13 @@ public class Calculator
 
     public void Calculate(){
         // Add your code here
-        int Addition += 5;
+        Addition += 5;
 
-        int Subtraction -= 1;
+        Subtraction -= 1;
 
-        int Multiplication *= 7;
+        Multiplication *= 7;
 
-        int Division /= 2;
+        Division /= 2;
     }
 }
 
